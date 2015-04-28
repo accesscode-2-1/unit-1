c89f389127f1aafd3e67c151c4b39b15508c660b
John Rodriguez <john.rodriguez@gmail.com>
2015-04-28 12:25:55 -0400

    Update Generics.java

--- review/2015-04-27/src/Generics.java
+++ review/2015-04-27/src/Generics.java
@@ -5,7 +5,7 @@ public class Generics {
         HashMap<String, Foo> map1 = new HashMap<String, Foo>();
         HashMap<String, Bar> map2 = new HashMap<String, Bar>();
 
-        // This is the signature/defintion of HashMap's putAll method:
+        // This is the signature/definition of HashMap's putAll method:
         // public void putAll(Map<? extends K, ? extends V> m)
 
         // this line will not compile, since Foo is not necessarily a Bar
