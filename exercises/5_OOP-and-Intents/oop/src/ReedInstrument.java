b7ca29b78bd2b3afc97e0e1e111bddd5f1bdcde6
theaulait <vanice.yee@gmail.com>
2015-04-30 21:56:13 -0400

    exercises

---
+++ exercises/5_OOP-and-Intents/oop/src/ReedInstrument.java
@@ -0,0 +1,16 @@
+/**
+ * Created by c4q-vanice on 4/30/15.
+ */
+public abstract class ReedInstrument implements Instrument {
+    boolean reed;
+
+    public void hasReed(){
+
+    }
+    public void placeReed(){
+
+    }
+    public void removeReed(){
+
+    }
+}
