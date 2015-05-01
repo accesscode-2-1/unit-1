9c125a9733a81d706d5e514ec02bc2be4fa8b545
Luke Desktop <lukesterlee@gmail.com>
2015-05-01 11:43:35 -0400

    homework 1,2

---
+++ homework/src/Card.java
@@ -0,0 +1,18 @@
+package nyc.c4q.lukesterlee;
+
+/**
+ * Created by Luke Lee on 4/27/2015.
+ */
+public class Card {
+
+    int number;
+    String shape;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+}
