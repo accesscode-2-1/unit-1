d8bbc519da144094a67b9625a263169533ecfecf
sufeiiz <szhao2@gmail.com>
2015-04-30 21:59:52 -0400

    exercises

---
+++ exercises/5_OOP-and-Intents/MyPhone/app/src/main/java/com/example/accesscode/myphone/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.accesscode.myphone;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* DialerActivity */
+        Button dialerButton = (Button) findViewById(R.id.dialer_button);
+        dialerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Dialer clicked", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), DialerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /* EmailActivity */
+        Button emailButton = (Button) findViewById(R.id.email_button);
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Email clicked", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), EmailActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
