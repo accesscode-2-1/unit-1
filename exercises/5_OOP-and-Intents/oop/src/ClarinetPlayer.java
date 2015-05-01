d5489aadf2997d705912b0c4cc1c89bcc6026de1
charlyn23 <charlyn.buchanan@gmail.com>
2015-05-01 19:11:23 -0400

    initial commit, wip

---
+++ exercises/5_OOP-and-Intents/oop/src/ClarinetPlayer.java
@@ -0,0 +1,20 @@
+/**
+ * Created by charlynbuchanan on 4/30/15.
+ */
+public class ClarinetPlayer extends Musician
+{
+    @Override
+    public String play_instrument()
+    {
+        return null;
+    }
+
+    public ClarinetPlayer(Clarinet c) {
+        c = new Clarinet();
+        Clarinet myClarinet = new Clarinet();
+    }
+
+    public void toggle_reed() {
+        
+    }
+}
---
+++ exercises/5_OOP-and-Intents/oop/src/ReedInstrument.java
@@ -0,0 +1,22 @@
+/**
+ * Created by charlynbuchanan on 4/30/15.
+ */
+public abstract class ReedInstrument implements Instrument {
+
+    boolean reed;
+    public boolean has_reed(Clarinet c){
+        if (c.reed = true)
+        return true;
+        else
+            return false;
+    }
+
+    public void remove_reed() {
+        if (reed = true){
+            reed = false;
+        }
+    }
+
+
+
+}
