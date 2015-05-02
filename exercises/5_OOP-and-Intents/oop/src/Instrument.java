2fe2d3b935352fb4ca0ef9766b878500e8a0e3cd
abassawo <abass.bayo@gmail.com>
2015-05-02 16:44:49 -0400

    Added instrument and musician class

---
+++ exercises/5_OOP-and-Intents/oop/src/Instrument.java
@@ -0,0 +1,7 @@
+/**
+ * Created by amyquispe on 4/30/15.
+ */
+public interface Instrument {
+    /* expected behavior: when play() is called, return a String that represents the Instrument's noise */
+    public String play();
+}
