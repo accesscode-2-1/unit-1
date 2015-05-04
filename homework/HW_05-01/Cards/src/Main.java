a954da8093d43ad2d7c9eb961d361603a8d8d1b0
KMaragh <ksmaragh@gmail.com>
2015-05-04 00:04:44 -0400

    Added some tests to my text adventure game, still need to complete

---
+++ homework/HW_05-01/Cards/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by kadeemmaragh on 5/1/15.
+ * Create an abstract class representing a game, which includes the method getpieces().
+ Create a class that extends your Game class, PlayingCards.
+ Create a class, Card, representing a playing card.
+ In your constructor for PlayingCards, genereate a 52-deck hand of Cards. (Do not write 52 lines of code to do this). Decide an appropriate collection to store these in.
+ Implement getpieces() which should return your collection of Cards. What should be the type signature for getpieces() in the abstract class?
+ */
+public class Main
+{
+}
---
+++ homework/HW_05-01/TextAdventure/src/Greeting.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kadeemmaragh on 5/3/15.
+ */
+public interface Greeting
+{
+    public void greeting();
+    public void farewell();
+
+}
