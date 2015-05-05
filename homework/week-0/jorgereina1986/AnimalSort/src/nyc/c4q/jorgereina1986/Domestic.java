0ffe30c56b77f407103bd977ba1162191606ae1d
Jorge Reina <jorgereina1986@gmail.com>
2015-05-04 23:38:56 -0400

    AnimalSort in progress

---
+++ homework/week-0/jorgereina1986/AnimalSort/src/nyc/c4q/jorgereina1986/Domestic.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jorge Reina on 5/4/15.
+ */
+public interface Domestic
+{
+    String getName();
+    void setName(String name);
+}
---
+++ homework/week-0/jorgereina1986/AnimalSort/src/nyc/c4q/jorgereina1986/DomesticCat.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Jorge Reina on 5/4/15.
+ */
+public class DomesticCat extends Animal implements Domestic
+{
+    @Override
+    public String getName()
+    {
+        return null;
+    }
+
+    @Override
+    public int compareTo(Animal o)
+    {
+        return super.compareTo(o);
+    }
+
+    @Override
+    public void setName()
+    {
+
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        return super.equals(obj);
+    }
+
+    public DomesticCat()
+    {
+    }
+}
