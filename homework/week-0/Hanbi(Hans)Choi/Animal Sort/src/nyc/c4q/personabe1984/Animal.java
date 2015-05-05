e8f8d8ac1a4be55fb750c929fcf045d35616ee5d
personabe1984 <personabe1984@gmail.com>
2015-05-04 22:51:14 -0400

    First commit

---
+++ homework/week-0/Hanbi(Hans)Choi/Animal Sort/src/nyc/c4q/personabe1984/Animal.java
@@ -0,0 +1,9 @@
+package nyc.c4q.personabe1984;
+/**
+ * Created by Hans on 5/4/15.
+ */
+public class Animal
+{
+    private String animalName;
+    
+}
---
+++ homework/week-0/Hanbi(Hans)Choi/Animal Sort/src/nyc/c4q/personabe1984/Main.java
@@ -0,0 +1,21 @@
+package nyc.c4q.personabe1984;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        /***
+         * Create an Animal class that includes a "species name" field and implements the Comparable interface.
+         * Compare two animals by species name, alphabetically.
+         * Implement equals to compare by species name.
+         Test your compareTo function by creating an array of Animal instances and calling sort.
+         Create the Domestic interface. This interface should include getters and setters for a name field, since pets tend to have their own names.
+         Create a class for a Domestic Animal, perhaps a DomesticCat, that both inherits from Animal and implements the Domesitc interface.
+         Implement the compareTo function for your Domestic animal so that it compares two such animals by their given name instead of their species name.
+         Implement equals to compare by given name.
+         Create a constructor for your Domestic animal that takes in both the species name and given name.
+         Test your compareTo function by creating an array and calling sort.
+          */
+    }
+}
