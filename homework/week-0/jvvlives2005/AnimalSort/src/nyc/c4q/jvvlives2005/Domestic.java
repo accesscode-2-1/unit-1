923a968ea0d689bd09e761d68040af48f925a627
jvvlives2005 <jvvlives2005@aol.com>
2015-05-07 21:23:00 -0400

    Homework Animal Sort Completed

---
+++ homework/week-0/jvvlives2005/AnimalSort/src/nyc/c4q/jvvlives2005/Domestic.java
@@ -0,0 +1,16 @@
+package nyc.c4q.jvvlives2005;
+import javax.xml.soap.Text;
+
+/**
+ * Created by c4q-joshelynvivas on 5/5/15.
+ */
+public interface Domestic
+{
+    void speciesName();
+
+    String getName();
+
+    void setName(String name);
+
+
+}
