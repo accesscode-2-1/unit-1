cb2015ef2e66e95b7e2874f963df51b9093c93e4
jvvlives2005 <jvvlives2005@aol.com>
2015-05-09 14:20:09 -0400

    initial commit--IGNORE THE FILES

---
+++ homework/week-1/HoroscopeHomework/app/build/generated/source/buildConfig/debug/jvvlives2005/c4q/nyc/horoscopehomework/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jvvlives2005.c4q.nyc.horoscopehomework;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "jvvlives2005.c4q.nyc.horoscopehomework";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
