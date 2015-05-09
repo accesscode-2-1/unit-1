e1a11f21c41dac5427557c97bed8bc3e53ce9840
KMaragh <KMaragh@users.noreply.github.com>
2015-05-09 11:29:47 -0400

    Updated compareTo method

--- homework/HW_05-01/AnimalSort/src/Animal.java
+++ homework/HW_05-01/AnimalSort/src/Animal.java
@@ -11,19 +11,8 @@ public class Animal implements Comparable<Animal>
     @Override
     public int compareTo(Animal o)
     {
-        if(this.speciesName.toLowerCase().compareTo(o.speciesName.toLowerCase()) < 0)
-        {
-            return - 1; //if the object animal's name is alphabetically before this's name
-        }
-        else if(this.speciesName.toLowerCase().compareTo(o.speciesName.toLowerCase()) == 0)
-        {
-            return 0; //If they are the same
-        }
-        else
-        {
-            return 1; // if the object animal's name is alphabetically after this's name
-        }
-
+       return this.speciesName.toLowerCase().compareTo(o.speciesName.toLowerCase());
+       
     }
 
     public boolean equals(Animal ani)
