581a84695d724b59c4ba06165e99ce29de102aeb
theaulait <vanice.yee@gmail.com>
2015-05-08 21:11:23 -0400

    initial

---
+++ homework/Cards/src/Game.java
@@ -0,0 +1,14 @@
+/**
+ * Created by c4q-vanice on 5/4/15.
+ */
+public abstract class Game {
+    private int piece;
+
+    public void setPiece(int piece){
+        this.piece = piece;
+    }
+
+    public int getPieces(){
+        return piece;
+    }
+}
--- homework/HW_05-01/src/Animal.java
+++ homework/HW_05-01/src/Animal.java
@@ -1,35 +1,28 @@
-public class Animal implements Comparable {
+public class Animal implements Comparable<Animal> {
     private String speciesName;
     private Object animal;
 
-    public Animal(String speciesName){
+    public Animal(String speciesName) {
 
     }
 
-    public void setSpecie(String specie){
+    public void setSpecie(String specie)
+    {
         speciesName = specie;
     }
 
-    public String getSpecie(){
+    public String getSpecie()
+    {
         return speciesName;
     }
 
     @Override
-    public int compareTo(Object animal)
-    {
-        return 0;
+    public int compareTo(Animal otheranimal) {
+        if(speciesName.equals(otheranimal.speciesName)){
+            speciesName.compareTo(otheranimal.speciesName);
+        } return  0;
     }
 }
-
-//    @Override
-//    public int compareTo(Animal animal) {
-//        super(speciesName);
-//        if(speciesName.equals(animal.speciesName)) {
-//            return speciesName.compareTo(animal.speciesName);
-//        }
-//        else
-//            return 0;
-//    }
 // I am commenting this section out because it does not work and I am trying to find a way to make it work.
 
 // I do not understand generic and will try to find the solution.
