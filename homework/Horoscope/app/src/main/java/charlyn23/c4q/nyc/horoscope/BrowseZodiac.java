c4c9d83f00f080cec667eecfcf52dc125622a85e
charlyn23 <charlyn.buchanan@gmail.com>
2015-05-08 22:38:18 -0400

    initial commit

---
+++ homework/Horoscope/app/src/main/java/charlyn23/c4q/nyc/horoscope/BrowseZodiac.java
@@ -0,0 +1,26 @@
+package charlyn23.c4q.nyc.horoscope;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by charlynbuchanan on 5/7/15.
+ */
+public class BrowseZodiac extends android.support.v4.app.Fragment implements View.OnClickListener {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_browse_zodiac,  container, false);
+        //return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
