d1e6cc1f403b497234d05c24a17f3f92287acd6c
Luke Desktop <lukesterlee@gmail.com>
2015-05-13 12:57:03 -0400

    clean the project. (ignore this commit)

--- homework/lukesterlee/week-2/Horoscope/app/src/main/java/lukesterlee/c4q/nyc/horoscope/NavigationDrawerCallbacks.java
+++
@@ -1,5 +0,0 @@
-package lukesterlee.c4q.nyc.horoscope;
-
-public interface NavigationDrawerCallbacks {
-    void onNavigationDrawerItemSelected(int position);
-}
--- homework/lukesterlee/week-2/Horoscope/app/src/main/java/lukesterlee/c4q/nyc/horoscope/SignItemCallbacks.java
+++
@@ -1,8 +0,0 @@
-package lukesterlee.c4q.nyc.horoscope;
-
-/**
- * Created by Luke on 5/13/2015.
- */
-public interface SignItemCallbacks {
-    void onSignItemSelected(int position);
-}
