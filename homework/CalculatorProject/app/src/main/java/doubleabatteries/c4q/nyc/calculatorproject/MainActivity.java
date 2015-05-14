fc00480623b734ec10ec712c9b37b5536dbe4507
ahhhlvin <alvkuang@gmail.com>
2015-05-13 20:59:09 -0400

    calculator updates -0513

--- homework/CalculatorProject/app/src/main/java/doubleabatteries/c4q/nyc/calculatorproject/MainActivity.java
+++ homework/CalculatorProject/app/src/main/java/doubleabatteries/c4q/nyc/calculatorproject/MainActivity.java
@@ -11,198 +11,220 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
+
+    TextView textview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textview = (TextView) findViewById(R.id.textView);
+        textview = (TextView) findViewById(R.id.textView);
+
+        if(savedInstanceState != null){
+            textview.setText(savedInstanceState.getString("equation"));
+        }
 
         Button button7 = (Button) findViewById(R.id.seven);
-        Button button8 = (Button) findViewById(R.id.eight);
-        Button button9 = (Button) findViewById(R.id.nine);
-        Button button4 = (Button) findViewById(R.id.four);
-        Button button5 = (Button) findViewById(R.id.five);
-        Button button6 = (Button) findViewById(R.id.six);
-        Button button1 = (Button) findViewById(R.id.one);
-        Button button2 = (Button) findViewById(R.id.two);
-        Button button3 = (Button) findViewById(R.id.three);
-        Button buttonParen = (Button) findViewById(R.id.parentheses);
-        Button button0 = (Button) findViewById(R.id.zero);
-        Button buttonDecimal = (Button) findViewById(R.id.decimal);
-        Button buttonClear = (Button) findViewById(R.id.clear);
-        Button buttonAdd = (Button) findViewById(R.id.add);
-        Button buttonSubtract = (Button) findViewById(R.id.subtract);
-        Button buttonNegative = (Button) findViewById(R.id.negative);
-        Button buttonBackspace = (Button) findViewById(R.id.backspace);
-        Button buttonMultiply = (Button) findViewById(R.id.multiply);
-        Button buttonDivide = (Button) findViewById(R.id.divide);
-        Button buttonEnter = (Button) findViewById(R.id.enter);
-
-
         button7.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                textview.setText("7");
+                textview.append("7");
             }
         });
-
+        Button button8 = (Button) findViewById(R.id.eight);
         button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                textview.append("8");
             }
         });
-
+        Button button9 = (Button) findViewById(R.id.nine);
         button9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                textview.append("9");
             }
         });
-
+        Button button4 = (Button) findViewById(R.id.four);
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                textview.append("4");
             }
         });
-
+        Button button5 = (Button) findViewById(R.id.five);
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                int id = view.getId();
+                textview.append("5");
             }
         });
-
+        Button button6 = (Button) findViewById(R.id.six);
         button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                textview.append("6");
             }
         });
-
+        Button button1 = (Button) findViewById(R.id.one);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                textview.append("1");
             }
         });
-
+        Button button2 = (Button) findViewById(R.id.two);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                textview.append("2");
             }
         });
-
+        Button button3 = (Button) findViewById(R.id.three);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                textview.append("3");
             }
         });
 
+        // TODO!!
+        Button buttonParen = (Button) findViewById(R.id.parentheses);
         buttonParen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                textview.append("(");
             }
         });
-
+        Button button0 = (Button) findViewById(R.id.zero);
         button0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                textview.append("0");
             }
         });
 
+        // TODO !!
+        Button buttonDecimal = (Button) findViewById(R.id.decimal);
         buttonDecimal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                textview.append(".");
             }
         });
-
-        buttonBackspace.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonSubtract.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonNegative.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
+        Button buttonClear = (Button) findViewById(R.id.clear);
         buttonClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                textview.setText("");
             }
         });
+        Button buttonAdd = (Button) findViewById(R.id.add);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
 
+
+            @Override
+            public void onClick(View view) {
+                if (textview.getText().equals("")) {
+                } else if (!isOperator(textview.getText().toString())){
+                    textview.append(((Button) view).getText().toString());
+                }
+            }
+        });
+        Button buttonSubtract = (Button) findViewById(R.id.subtract);
+        buttonSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (textview.getText().equals("")) {
+                } else if (!isOperator(textview.getText().toString())){
+                    textview.append(((Button) view).getText().toString());
+                }            }
+        });
+        Button buttonNegative = (Button) findViewById(R.id.negative);
+        buttonNegative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textview.append("");
+            }
+        });
+        Button buttonBackspace = (Button) findViewById(R.id.backspace);
+        buttonBackspace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (textview.getText().toString().equals("")) {
+                } else {
+                    textview.setText(textview.getText().toString().substring(0, textview.getText().toString().length() - 1));
+                }
+            }
+        });
+        Button buttonMultiply = (Button) findViewById(R.id.multiply);
         buttonMultiply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-            }
+                if (textview.getText().equals("")) {
+                } else if (!isOperator(textview.getText().toString())){
+                    textview.append(((Button) view).getText().toString());
+                }            }
         });
-
+        Button buttonDivide = (Button) findViewById(R.id.divide);
         buttonDivide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-            }
+                if (textview.getText().equals("")) {
+                } else if (!isOperator(textview.getText().toString())){
+                    textview.append(((Button) view).getText().toString());
+                }            }
         });
-
+        Button buttonEnter = (Button) findViewById(R.id.enter);
         buttonEnter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+            // TODO: LIBRARY PARSE CODE GOES HERE !!!!
             }
         });
 
 
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("equation", textview.getText().toString());
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        super.onSaveInstanceState(outState);
+    }
+
+
+
+
+
+
+    public boolean isOperator(String input) {
+
+        if (textview.getText().toString().equals("")) {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        char lastC = input.charAt(input.length()-1);
+        char[] operators = {'+', '-', '*', '÷'};
+
+
+
+        for(char operator : operators){
+            if (lastC == operator) {
+                return true;
+            }
+        }
+
+        return false;
+
     }
+
+
+
+
+
 }
