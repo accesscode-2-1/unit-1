ad429b9a1735a240a31bd0b90fc06020bf3f5766
Amcbri15 <anthony.mcbride0711@gmail.com>
2015-05-15 13:31:56 -0400

    Initial Commit

---
+++ homework/AnthonyMcBride/Horoscope_Revisited/app/src/main/java/com/example/c4q_anthony_mcbride/horoscope_revisited/Aquarius.java
@@ -0,0 +1,15 @@
+package com.example.c4q_anthony_mcbride.horoscope_revisited;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class Aquarius extends ActionBarActivity{
+}
---
+++ homework/AnthonyMcBride/Horoscope_Revisited/app/src/main/java/com/example/c4q_anthony_mcbride/horoscope_revisited/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.c4q_anthony_mcbride.horoscope_revisited;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity{
+}
