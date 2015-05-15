6add809bbf35acc51e9672c66bd04e8df7fb4868
Luke Lee <samsung3star@gmail.com>
2015-05-15 16:33:22 -0400

    [1.3] added ActionBar title section

--- homework/lukesterlee/week-2/Horoscope/app/src/main/java/lukesterlee/c4q/nyc/horoscope/MainActivity.java
+++ homework/lukesterlee/week-2/Horoscope/app/src/main/java/lukesterlee/c4q/nyc/horoscope/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.FragmentTransaction;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -98,6 +99,12 @@ public class MainActivity extends ActionBarActivity
             super.onBackPressed();
     }
 
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -106,6 +113,7 @@ public class MainActivity extends ActionBarActivity
             // if the drawer is not showing. Otherwise, let the drawer
             // decide what to show in the action bar.
             getMenuInflater().inflate(R.menu.main, menu);
+            restoreActionBar();
             return true;
         }
         return super.onCreateOptionsMenu(menu);
