1f422d85725a0756e314533b33463707ed206f48
annalinewyork <annalinewyork@gmail.com>
2015-05-15 17:05:18 -0400

    created 4 activities and 4 layout xml files

---
+++ homework/week-1/annalinewyork/Horoscope/app/src/main/java/annalinewyork/c4q/nyc/horoscope/GameActivity.java
@@ -0,0 +1,15 @@
+package annalinewyork.c4q.nyc.horoscope;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by c4q-nali on 5/15/15.
+ */
+public class GameActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
