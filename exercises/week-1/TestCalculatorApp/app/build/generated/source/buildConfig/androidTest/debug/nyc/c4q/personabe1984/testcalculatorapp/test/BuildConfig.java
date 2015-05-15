a0eed30d08cccb17934b0b819b52b14774f184cc
personabe1984 <personabe1984@gmail.com>
2015-05-15 14:19:22 -0400

    horoscope app

---
+++ exercises/week-1/TestCalculatorApp/app/build/generated/source/buildConfig/androidTest/debug/nyc/c4q/personabe1984/testcalculatorapp/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package nyc.c4q.personabe1984.testcalculatorapp.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "nyc.c4q.personabe1984.testcalculatorapp.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
