5730de166182b10acc19b701a761c8c2c3a40ae6
Sea <janneisyvidals@gmail.com>
2015-05-16 00:45:54 -0400

    created 4 activities and 4 layout xml files

---
+++ homework/week-2/jvidals1/Horoscope/app/src/main/java/jvidals1/c4q/nyc/horoscope/GameActivity.java
@@ -0,0 +1,15 @@
+package jvidals1.c4q.nyc.horoscope;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by s3a on 5/16/15.
+ */
+public class GameActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
