5b6a65bc5eb7f0ea6c5e05ed4132233cba4cdc81
Amcbri15 <anthony.mcbride0711@gmail.com>
2015-05-15 21:58:50 -0400

    Partially Recovered Project

--- homework/AnthonyMcBride/Horoscope_Revisited/app/src/main/java/com/example/c4q_anthony_mcbride/horoscope_revisited/Sagittarius.java
+++ homework/AnthonyMcBride/Horoscope_Revisited/app/src/main/java/com/example/c4q_anthony_mcbride/horoscope_revisited/Sagittarius.java
@@ -12,4 +12,55 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 public class Sagittarius extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sagittarius_frame);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.sagittarius_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.navigator) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.sagittarius_fragment, container, false);
+            return rootView;
+        }
+    }
 }
