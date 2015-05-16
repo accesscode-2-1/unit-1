7be1ebe24f9fde82a134bd1cacaaafe9076f6975
jvvlives2005 <jvvlives2005@aol.com>
2015-05-15 22:16:20 -0400

    HW 20150515

---
+++ homework/week-1/HoroscopeHomework/app/src/main/java/jvvlives2005/c4q/nyc/horoscopehomework/app/src/androidTest/java/jvvlives2005/c4q/nyc/horoscopehomework/ApplicationTest.java
@@ -0,0 +1,13 @@
+package jvvlives2005.c4q.nyc.horoscopehomework;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
